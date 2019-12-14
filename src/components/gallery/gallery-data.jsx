5b966eeab72fd28c10b88c64db7f57619998527a
etriv <itai.riv@gmail.com>
2019-12-14 12:03:00 +0100

    Added gallery data and started working on gallery visuals

---
+++ src/components/gallery/gallery-data.jsx
@@ -0,0 +1,79 @@
+// TODO: Use youTube's api to get the thumbnail of a resourse when submitted
+// * Sometimes there might no be a "maxres" (HD) image
+
+const galleryData = [
+    {
+        id: 1,
+        title: "Amazing group dance by Crystal Pite",
+        text: "This original choreography really inspires me",
+        image: "https://img.youtube.com/vi/_QBrqAbMJJ8/maxresdefault.jpg",
+        source: "https://youtu.be/_QBrqAbMJJ8",
+        likes: 5,
+        dateAdded: "1/12/19"
+    },
+    {
+        id: 2,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 7,
+        dateAdded: "3/12/19"
+    },
+    {
+        id: 3,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 3,
+        dateAdded: "5/12/19"
+    },
+    {
+        id: 4,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 17,
+        dateAdded: "10/12/19"
+    },
+    {
+        id: 5,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 13,
+        dateAdded: "13/12/19"
+    },
+    {
+        id: 6,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 15,
+        dateAdded: "13/12/19"
+    },
+    {
+        id: 7,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 8,
+        dateAdded: "14/12/19"
+    },
+    {
+        id: 8,
+        title: "A dance with objects",
+        text: "This choreography is awesome!",
+        image: "https://img.youtube.com/vi/MbVwAZHeX2o/maxresdefault.jpg",
+        source: "https://youtu.be/MbVwAZHeX2o",
+        likes: 2,
+        dateAdded: "14/12/19"
+    }
+]
+
+export default galleryData;
--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './gallery.scss'
 import GalleryFilters from '../gallery-filters/gallery-filters'
+import galleryData from './gallery-data';
 
-const Gallery = () => {
-    return (
-        <div className="gallery-container">
-            <GalleryFilters />
-            <p>Gallery items...</p>
-        </div>
-    );
+class Gallery extends Component {
+    state = {
+        items: galleryData
+    }
+    
+    render() {
+        console.log(this.state.items);
+        return (
+            <div className="gallery-container">
+                <GalleryFilters />
+                <p>Gallery items...</p>
+            </div>
+        );
+    }
 }
 
 export default Gallery;
