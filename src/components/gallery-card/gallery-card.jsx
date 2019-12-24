eab8152f596af4f7dd44154c90937da43095bbb7
etriv <itai.riv@gmail.com>
2019-12-24 13:13:12 +0200

    Card is a link using a href instead of JS

--- src/components/gallery-card/gallery-card.jsx
+++ src/components/gallery-card/gallery-card.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './gallery-card.scss'
 
-const cardClickHandler = (url) => {
-    console.log('Card clicked.');
-    window.open(url, '_blank');
-}
+// const cardClickHandler = (url) => {
+//     console.log('Card clicked.');
+//     window.open(url, '_blank');
+// }
 
 const GalleryCard = (props) => {
     //console.log(props);
     return (
-        <div className="card" onClick={() => cardClickHandler(props.source)}>
-            <div className="card-image"
-            style={{
-                backgroundImage: `url(${props.image})`
-            }}
-            alt="" />
-            <div className="card-desc">
-                <p className="title">{props.title}</p>
-            </div>
+        <div className="card">
+            <a href={props.source} className="card-content" target="_blank" rel="noopener noreferrer">
+                    <div className="card-image"
+                    style={{
+                        backgroundImage: `url(${props.image})`
+                    }}
+                    alt="" />
+                    <div className="card-desc">
+                        <p className="title">{props.title}</p>
+                    </div>
+            </a>
         </div>
     );
 }
