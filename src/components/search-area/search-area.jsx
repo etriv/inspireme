632e0fdd40945d33fe34dbe93769adbf450cd16d
etriv <itai.riv@gmail.com>
2020-01-07 15:32:31 +0100

    Searching by tags and pressing ENTER

--- src/components/search-area/search-area.jsx
+++ src/components/search-area/search-area.jsx
@@ -5,24 +5,36 @@ class SearchArea extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            search_field: ''
+            search_field: '',
         };
-
+        
         this.onSearchBoxChange = this.onSearchBoxChange.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
     }
 
     onSearchBoxChange(event) {
-        this.setState({search_field: event.target.value}, () => {
-            console.log(this.state.search_field);
+        this.setState({ search_field: event.target.value }, () => {
+            // console.log(this.state.search_field);
         });
+    }
+
+    handleKeyUp(event) {
+        // If ENTER is being pressed, initiate fetching of inspirations
+        if (event.keyCode === 13) {
+            this.props.inspireOnClick(this.state.search_field, '');
+        }
     }
 
     render() {
         return (
             <div className="search-area-container">
                 <p className="inspire-text">Inspire me to:</p>
-                <input className="search-box" type="text" name="search-text" onChange={this.onSearchBoxChange} placeholder="✎" />
-                <button className="button" onClick={this.props.inspireOnClick.bind(this, 'TAGS', 'TYPE')}>Inspire</button>
+                <input name="search-text" className="search-box" type="text" placeholder="✎"
+                    onChange={this.onSearchBoxChange}
+                    onKeyUp={this.handleKeyUp} />
+                <button className="button"
+                    onClick={this.props.inspireOnClick.bind(this, this.state.search_field, '')}>
+                    Inspire</button>
             </div>
         );
     }
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -5,9 +5,25 @@ import SearchArea from '../../components/search-area/search-area';
 import Gallery from '../../components/gallery/gallery';
 
 async function getInspirationsFromDB(tags = '', type = '') {
-    console.log('Getting inspirations from DB ()...', tags, type);
-    const data = await fetch('http://localhost:3001/inspirations/')
-        .then(response => response.json())
+    console.log('Getting inspirations from DB ()...', 'tags:', tags, 'type:', type);
+    let fetchUrl = 'http://localhost:3001/inspirations/';
+    if (tags !== '' || type !== '') {
+        fetchUrl += '?';
+        fetchUrl += tags !== '' ? '&tags=' + tags : '';
+        fetchUrl += type !== '' ? '&type=' + type : '';
+    }
+    console.log('Fetching:', fetchUrl);
+    let data = await fetch(fetchUrl)
+        .then(response => {
+            if (response.status === 200)
+                return response.json();
+            console.log('Response status is not OK');
+            return [];
+        })
+        .catch((e) => {
+            console.log('Error while fetching inspirations:', e);
+            return [];
+        })
     return data;
 }
 
@@ -22,7 +38,7 @@ class HomePage extends React.Component {
     }
 
     updateInspirations(tags = '', type = '') {
-        console.log('Updateing inspirations...', 'tags: ', tags, 'type: ', type);
+        console.log('Updateing inspirations...', 'tags:', tags, 'type:', type);
         getInspirationsFromDB(tags, type)
             .then(data => this.setState({inspirations: data}));
     }
