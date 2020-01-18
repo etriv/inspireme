0e0c377c2be2e373c2e9d645800c59f456cea3be
etriv <itai.riv@gmail.com>
2020-01-18 20:48:42 +0100

    Added FadeIn to gallery-filters. Ended up copying the lib's code and made change to the animation's direction.

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -3,13 +3,16 @@ import './gallery.scss'
 import GalleryFilters from '../gallery-filters/gallery-filters'
 // import galleryData from './gallery-data';
 import GalleryCard from '../gallery-card/gallery-card';
+import FadeIn from "../fade-in/fade-in";
 
 class Gallery extends Component {
     render() {
         // console.log('Gallery items: ', this.props.items);
         return (
             <div className="gallery-container">
-                <GalleryFilters onFilterChange={this.props.onFilterChange} />
+                <FadeIn className='filters-style'>
+                    <GalleryFilters onFilterChange={this.props.onFilterChange} />
+                </FadeIn>
                 <div className="cards-container">
                 {
                     this.props.items.map((item) => {
